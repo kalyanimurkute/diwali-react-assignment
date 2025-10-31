69d8d9064249161744ba354c68ab896e2a6a2ac6
kalyanimurkute <murkutekalyani259@gmail.com>
2025-10-31 13:14:36 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './home.jsx'
+import Home from './Home.jsx'
 import Contact from './Contact.jsx'
 import About from './About.jsx'
 
