1e45dd29bf8c94f2f93a08c6312ca3d2868225cc
kalyanimurkute <murkutekalyani259@gmail.com>
2025-11-01 12:57:17 +0530

    added file

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div>
          <Navbar />
          <h1>Welcome to Diwali Vibes – Let the Light Shine Bright!</h1>
-         <p>Celebrate Diwali with lights, sweets, and joyful moments — Diwali vibes all around. Join us in sharing warmth, color, and happiness with family and friends.</p>
+         <p className="diwali-para">Celebrate Diwali with lights, sweets, and joyful moments — Diwali vibes all around. Join us in sharing warmth, color, and happiness with family and friends.</p>
          </div>
   )
 }
